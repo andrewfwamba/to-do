f3606422c3085d63d131fc3bf16dad17b87b2e41
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-11-20 23:21:15 +0300

    task: new todo

---
+++ src/app/new/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Page() {
+  return (
+    <>
+      <header className="w-full flex justify-between items-center mb-6 py-6">
+        <h1 className="text-2xl">New</h1>
+      </header>
+      <form action=""></form>
+      <h1></h1>
+    </>
+  );
+}
