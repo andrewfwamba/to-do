d70b6bf4c33b408869ed6ae72e8d70bec632d109
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-11-21 14:35:32 +0300

    task: new todos

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,4 +1,19 @@
+import Link from "next/link";
 import React from "react";
+import { prisma } from "../db";
+import { redirect } from "next/navigation";
+
+async function createTodo(data: FormData) {
+  "use server";
+
+  const title = data.get("title")?.valueOf();
+  if (typeof title !== "string" || title.length === 0) {
+    throw new Error("Invalid Title");
+  }
+
+  await prisma.todo.create({ data: { title, complete: false } });
+  redirect("/");
+}
 
 export default function Page() {
   return (
@@ -6,7 +21,27 @@ export default function Page() {
       <header className="w-full flex justify-between items-center mb-6 py-6">
         <h1 className="text-2xl">New</h1>
       </header>
-      <form action=""></form>
+      <form action={createTodo} className="flex flex-col gap-4">
+        <input
+          type="text"
+          name="title"
+          className="border border-slate-300 bg-transparent rounded px-2 py-1 outline-none focus-within:border-slate-100"
+        />
+        <div className="flex gap-4 justify-end">
+          <Link
+            href=".."
+            className="outline-none border text-slate-300 border-slate-300 rounded px-2 py-1 hover:bg-slate-700 focus-within:bg-slate-700"
+          >
+            Cancel
+          </Link>
+          <button
+            type="submit"
+            className="outline-none border text-slate-300 border-slate-300 rounded px-2 py-1 hover:bg-slate-700 focus-within:bg-slate-700"
+          >
+            Create
+          </button>
+        </div>
+      </form>
       <h1></h1>
     </>
   );
